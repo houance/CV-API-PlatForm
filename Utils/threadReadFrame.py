413e99276a5f2e867a50f7119d49134d5755245d
houance <837507557@qq.com>
2020-12-19 14:01:56 +0800

    add threading read frame class

---
+++ Utils/threadReadFrame.py
@@ -0,0 +1,44 @@
+from threading import Thread
+import cv2
+from queue import Queue
+import time
+
+
+class streamer:
+    def __init__(self, path, queueSize=128):
+        self.cap = cv2.VideoCapture(path)
+        self.queue = Queue(maxsize=queueSize)
+        self.stop = False
+        self.startThread()
+        time.sleep(1)
+
+    def startThread(self):
+        t = Thread(target=self.readFrame, args=())
+        t.daemon = True
+        t.start()
+        return self
+
+    def readFrame(self):
+        while True:
+            if self.stop:
+                return
+
+            if not self.queue.full():
+                ret, frame = self.cap.read()
+
+                if not ret:
+                    self.stop = True
+                    self.cap.release()
+                    break
+                self.queue.put(frame)
+            else:
+                continue
+
+    def hasMore(self):
+        if not self.queue.empty():
+            return True
+        else:
+            return False
+
+    def getFrame(self):
+        return self.queue.get()
