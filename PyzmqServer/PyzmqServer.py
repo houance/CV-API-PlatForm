c47636d93aee0140c9be489b052cebf4c71f947a
houance <837507557@qq.com>
2021-01-21 22:10:47 +0800

    fix multiprocessing bug for c extension(zmq)

--- PyzmqServer/PyzmqServer.py
+++ PyzmqServer/PyzmqServer.py
@@ -1,17 +1,18 @@
-import zmq
+import zmq.green as zmq
 from LoadBalance import LoadBalance
 from CVServer.CascadesDetector import CascadesDetector
 from CVServer.YOLOV3 import YOLO
 from CVServer.yuNet import yuNet
 from multiprocessing import Process
-from threading import Thread
+import time
+
 
 
 class PyzmqServer:
     def __init__(self, context, address, method):
-        self.socket = context.socket(zmq.REP)
-        self.socket.bind(address)
+        self.address = address
         self.method = method
+        self.socket = None
         self.loadBalance = LoadBalance(cpu=80, gpu=70)
         if method == 'Yolo':
             self.cvService = YOLO(gpu=1)
@@ -19,22 +20,25 @@ class PyzmqServer:
             self.cvService = CascadesDetector()
         elif method == 'YuNet':
             self.cvService = yuNet()
-        self.start()
+        self.startProcess(context)
 
-    def service(self):
+    def service(self, context):
+        self.socket = context.socket(zmq.REP)
+        self.socket.bind(self.address)
         while True:
+            time.sleep(0.1)
             recv = self.socket.recv(copy=False)
             if len(recv) == 2:
-                self.AnswerServerUp()
+                self.answerServerUp()
             elif len(recv) == 3:
-                self.LoadBalance()
+                self.answerServerBusy()
             else:
                 self.socket.send_pyobj(self.cvService.PyzmqPredict(recv), copy=False)
 
-    def AnswerServerUp(self):
+    def answerServerUp(self):
         self.socket.send_string('server up')
 
-    def LoadBalance(self):
+    def answerServerBusy(self):
         if self.method == 'YuNet':
             if self.loadBalance.CpuBusy():
                 self.socket.send_string('not ok')
@@ -46,5 +50,5 @@ class PyzmqServer:
             else:
                 self.socket.send_string('ok')
 
-    def start(self):
-        Thread(target=self.service()).start()
+    def startProcess(self, context):
+        Process(target=self.service, args=(context,)).start()
